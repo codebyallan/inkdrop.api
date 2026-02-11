24b944af539a2d19436e05d38ee34013b6526b9f
codebyallan <allan.pereira.dev@proton.me>
2026-02-11 20:18:44 -0300

    feat(migrations): add Movements table

---
+++ Inkdrop.Api/Migrations/20260211231717_AddMovementsEntity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inkdrop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovementsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TonerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PrinterId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movements_Printers_PrinterId",
+                        column: x => x.PrinterId,
+                        principalTable: "Printers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Movements_Toners_TonerId",
+                        column: x => x.TonerId,
+                        principalTable: "Toners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movements_PrinterId",
+                table: "Movements",
+                column: "PrinterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movements_TonerId",
+                table: "Movements",
+                column: "TonerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movements");
+        }
+    }
+}
