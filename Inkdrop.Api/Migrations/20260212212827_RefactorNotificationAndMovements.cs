39bc7da321255a3f4e80dbbb67fc914f148a975b
codebyallan <allan.pereira.dev@proton.me>
2026-02-12 19:00:23 -0300

    feat(migrations): refactor movements schema and notification logic

---
+++ Inkdrop.Api/Migrations/20260212212827_RefactorNotificationAndMovements.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inkdrop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorNotificationAndMovements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
