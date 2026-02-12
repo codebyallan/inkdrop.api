a0bf8916d6f16cf30f7eeb77716787a7e3eba853
codebyallan <allan.pereira.dev@proton.me>
2026-02-12 13:50:59 -0300

    feat(migrations): remove UpdatedAt column from Movements table

---
+++ Inkdrop.Api/Migrations/20260212164938_RemoveUpdatedAtFromMovements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inkdrop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUpdatedAtFromMovements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Movements");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Movements",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
